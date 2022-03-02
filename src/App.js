842ca7f7f0d6cfeaa8ebb2700cc64e6acbe9679a
michellechoi126 <michellechoi126@gmail.com>
2022-03-02 14:38:20 -0800

    imported components into App.js and established preliminary Routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import DogDetails from './components/DogDetails/DogDetails';
+import DogsList from './components/DogsList/DogsList';
+
 
 function App() {
 	const [dogFacts, setDogFacts] = useState([]);
 	
-  function getDogData() {
-			const url = `https://api.thedogapi.com/v1/breeds?api_key=${process.env.REACT_APP_API_KEY}`;
-			fetch(url)
-				.then((res) => res.json())
-				.then((data) => {
-					console.log('We have data!', data);
-					setDogFacts(data);
-				})
-				.catch(console.error);
-		}
+	function getDogData() {
+		const url = `https://api.thedogapi.com/v1/breeds?api_key=${process.env.REACT_APP_API_KEY}`;
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log('We have data!', data);
+				setDogFacts(data);
+			})
+			.catch(console.error);
+	}
 		
 	useEffect(() => {
 		getDogData();
@@ -22,8 +27,14 @@ function App() {
 	return (
 		<div className='App'>
 			<header>
-        Hello! This is the App Component.
+				<Header />
 			</header>
+			<main>
+				<Routes>
+					<Route path='/' element={<DogsList />} />
+					<Route path='' element={<DogDetails />} />
+				</Routes>
+			</main>
 		</div>
 	);
 }
