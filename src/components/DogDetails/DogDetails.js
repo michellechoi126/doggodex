88a6b0b1c2f95b00d8c863c1c3df2b4f8e0f7ff5
michellechoi126 <michellechoi126@gmail.com>
2022-03-04 22:04:46 -0800

    added button functionality to dog details page

--- src/components/DogDetails/DogDetails.js
+++ src/components/DogDetails/DogDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer} from 'react';
 import { useParams } from 'react-router-dom';
 import DogImages from "../DogImages/DogImages";
 import "./DogDetails.css";
@@ -9,7 +9,6 @@ function DogDetails(props) {
     const { name } = useParams();
 
     const [dogDetails, setDogDetails] = useState(null);
-    const [moreDetails, setMoreDetails] = useState(false);
 	
 	function getDogData() {
 		const url = `https://api.thedogapi.com/v1/breeds/search?q=${name}`;
@@ -24,25 +23,24 @@ function DogDetails(props) {
 	useEffect(() => {
 		getDogData();
 	}, []);
-
-    function handleClick (event) {
-        (moreDetails ? setMoreDetails(false) : setMoreDetails(true))
-    };
     
-    function toggle() {
-        return(moreDetails ?
-            <>
-                <button onClick={handleClick}>More Details</button>
-                <h3>Origin: {(dogDetails[0].origin) ? (dogDetails[0].origin) : "unknown"}</h3>
-                <h3>Height: {(dogDetails[0].height.imperial) ? `${dogDetails[0].height.imperial} inches at the withers` : "unknown"}</h3>
-                <h3>Bred for: {(dogDetails[0].bred_for) ? (dogDetails[0].bred_for) : "unknown"}</h3>
-                <h3>Breed group: {(dogDetails[0].breed_group) ? (dogDetails[0].breed_group) : "unknown"}</h3>
-            </>
-        :
-            <>
-                <button onClick={handleClick}>More Details</button>
-            </>
-        )
+    const [display, dispatch] = useReducer(detailButtons, "please click on the detail button below, you would to know more about.");
+
+    function detailButtons(state, detail) {
+        switch(detail.type) {
+            case "TEMPERAMENT":
+                return <h3>Temperament: {dogDetails[0].temperament}</h3>;
+            case "ORIGIN":
+                return <h3>Origin: {(dogDetails[0].origin) ? (dogDetails[0].origin) : "unknown"}</h3>;
+            case "BREDFOR":
+                return <h3>Bred for: {(dogDetails[0].bred_for) ? (dogDetails[0].bred_for) : "unknown"}</h3>;
+            case "BREEDGROUP":
+                return <h3>Breed group: {(dogDetails[0].breed_group) ? (dogDetails[0].breed_group) : "unknown"}</h3>;
+            case "LIFESPAN":
+                return <h3>Average life span: {dogDetails[0].life_span}</h3>;
+            default:
+                return state;
+        }
     }
 
 	if (dogDetails === null) {
@@ -50,17 +48,59 @@ function DogDetails(props) {
 	}
 
     return (
-        <div>
-            <DogImages id={dogDetails[0].id}/>
-            
-            <div className="main-details">
-                <h1>{dogDetails[0].name}</h1>
-                <h3>Temperament: {dogDetails[0].temperament}</h3>
-                <h3>Weight: {dogDetails[0].weight.imperial} lbs</h3>
-                <h3>Average life span: {dogDetails[0].life_span}</h3>
+        <div className="dogDetails-container">
+            <div className="img-name">
+                <div className="knobs">
+                    <div>🔘</div>
+                    <div>🔘</div>
+                </div>
+                <div className="dogDetails-img">
+                    <DogImages id={dogDetails[0].id}/>
+                </div>
+                <div className="knobs-container">
+                    <div className="redlight">
+                        <div>🔴</div>
+                    </div>
+                    <div className="longKnobs">
+                        <div className="longKnob1">🔘</div>
+                        <div className="longKnob2">🔘</div>
+                    </div>
+                </div>
+                <div className="bottom-imgName">
+                    <div className="dogDetails-name">
+                        <h1>{dogDetails[0].name}</h1>
+                    </div>
+                    <div className="heart">♡</div>
+                </div>
             </div>
             <hr />
-            {toggle()}
+            <div className="details-container">
+                <div className="more-details">
+                    <div className="main-details">
+                        {display}
+                    </div >
+                    <div className="buttons-container">
+                        <button className="details-buttons" onClick={() => dispatch({ type: "TEMPERAMENT"})}>
+                            temperament</button>
+                        <button className="details-buttons" onClick={() => dispatch({ type: "LIFESPAN"})}>
+                            life span</button>
+                        <button className="details-buttons" onClick={() => dispatch({ type: "ORIGIN"})}>
+                            origin</button>
+                        <button className="details-buttons" onClick={() => dispatch({ type: "BREEDGROUP"})}>
+                            breed group</button>
+                        <button className="details-buttons" onClick={() => dispatch({ type: "BREDFOR"})}>
+                            bred for</button>
+                    </div>
+                </div>
+                <div className="weight-height">
+                    <div className="weight">
+                        <h3>Weight: {dogDetails[0].weight.imperial} lbs</h3>
+                    </div>
+                    <div className="height">
+                        <h3>Height: {(dogDetails[0].height.imperial) ? `${dogDetails[0].height.imperial} inches at the withers` : "unknown"}</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
